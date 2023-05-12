7c4b31cda8f414f99858d5aa87b6a89786647f2f
ShevchukAV666 <666mog@mail.ru>
2023-05-12 17:12:47 +0500

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       // Author one = new Author("Маргарет", "Митчелл");
+        Author authorOne = new Author("Маргарет", "Митчелл");
+        Book GoneWithTheWind = new Book("Унесённые ветром", authorOne , 1936);
+
+        System.out.println("GoneWithTheWind.nameOfBook = " + GoneWithTheWind.getNameOfBook());
+
+        System.out.println("GoneWithTheWind.authorOfBook = " + authorOne.getFirstName()+ " " + authorOne.getLastName());
+
+        System.out.println("GoneWithTheWind.yearPublication = " + GoneWithTheWind.getYearPublication());
+
+        GoneWithTheWind.setYearPublication(2023);
+        System.out.println("GoneWithTheWind.getYearPublication() = " + GoneWithTheWind.getYearPublication());
+
+        System.out.println();
+
+        Author authorTwo = new Author("Джордж", "Оруэлл");
+        Book AnimalFarm = new Book("Скотный двор", authorTwo, 1945);
+
+
+        System.out.println("AnimalFarm.nameOfBook = " +AnimalFarm.getNameOfBook());
+
+        System.out.println("AnimalFarm.authorOfBook = " + authorTwo.getFirstName()+ " " + authorTwo.getLastName());
+
+        System.out.println("AnimalFarm.yearPublication = " + AnimalFarm.getYearPublication());
+
+        AnimalFarm.setYearPublication(2023);
+        System.out.println("AnimalFarm.getYearPublication() = " + AnimalFarm.getYearPublication());
+
     }
 }
+
+
+
+
